07e0636fb84854baf702609a71735d15f12dbc5d
wallarug <wallarug@coliemore.com.au>
2023-12-09 22:11:43 +1100

    Day 5 Part 2.  CPU bound solution (single core)

--- 5/activity2.py
+++ 5/activity2.py
@@ -10,11 +10,37 @@ from sys import exit
 DEBUG = False
 
 import os
+from threading import Thread
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_path)
 
 f = open("input.txt", "r")
+
+class SeedPair:
+    def __init__(self, start, rg):
+        self.start = start
+        self.end = start + rg
+        self.next = start
+
+    def in_range(self, value):
+        if value >= self.start and value <= self.end:
+            return True
+        else:
+            return False
+        
+    def get_next(self):
+        if self.next > self.end:
+            return None
+        else:
+            self.next += 1
+            return self.next - 1
+
+    def __str__(self):
+        return "start: " + str(self.start) + " end: " + str(self.end)
+
+    def __repr__(self):
+        return "start: " + str(self.start) + " end: " + str(self.end)
 
 class SpecialMap:
     def __init__(self, desc):
@@ -72,7 +98,7 @@ for line in f.readlines():
     # hardcode some of the information
     if line.startswith("seeds"):
         old_seeds = line.strip("\n").split(":")[1].split(" ")
-        print(old_seeds)
+        #print(old_seeds)
 
         # remove empty strings
         for x in old_seeds:
@@ -80,16 +106,13 @@ for line in f.readlines():
                 old_seeds.remove(x)
 
         old_seeds = [int(x) for x in old_seeds]
-        print(old_seeds)
+        #print(old_seeds)
 
         ## Seeds are now pairs
         for i in range(0, len(old_seeds), 2):
-            start_seed = old_seeds[i]
-            end_seed = start_seed + old_seeds[i+1]
-            for j in range(start_seed, end_seed):
-                seeds.append(j)
+            seeds.append(SeedPair(old_seeds[i], old_seeds[i+1]))
 
-        print(seeds)
+        #print(seeds)
         continue
 
     # if it is not a seed, it is a map
@@ -110,44 +133,46 @@ for line in f.readlines():
 # Phase 2 - actually working out the seed locations
 lowest_location = 999999999999999999999999999999999999
 lowest_seed = 0
+counter = 0
 
-for seed in seeds:
-    # first, we need to find the soil location
-    soil_location = maps["seed-to-soil"].find_destination(seed)
-    #print("seed: ", seed, "soil location: ", soil_location)
+for seed_pair in seeds:
+    counter += 1
+    print("Computing Seed Range ", counter)
+    for seed in range(seed_pair.start, seed_pair.end):
+        # first, we need to find the soil location
+        soil_location = maps["seed-to-soil"].find_destination(seed)
+        #print("seed: ", seed, "soil location: ", soil_location)
 
-    # next, we need to find the fertilizer location
-    fertilizer_location = maps["soil-to-fertilizer"].find_destination(soil_location)
-    #print("seed: ", seed, "fertilizer location: ", fertilizer_location)
+        # next, we need to find the fertilizer location
+        fertilizer_location = maps["soil-to-fertilizer"].find_destination(soil_location)
+        #print("seed: ", seed, "fertilizer location: ", fertilizer_location)
 
-    # next, we need to find the water location
-    water_location = maps["fertilizer-to-water"].find_destination(fertilizer_location)
-    #print("seed: ", seed, "water location: ", water_location)
+        # next, we need to find the water location
+        water_location = maps["fertilizer-to-water"].find_destination(fertilizer_location)
+        #print("seed: ", seed, "water location: ", water_location)
 
-    # next, we need to find the light location
-    light_location = maps["water-to-light"].find_destination(water_location)
-    #print("seed: ", seed, "light location: ", light_location)
+        # next, we need to find the light location
+        light_location = maps["water-to-light"].find_destination(water_location)
+        #print("seed: ", seed, "light location: ", light_location)
 
-    # next, we need to find the temperature location
-    temperature_location = maps["light-to-temperature"].find_destination(light_location)
-    #print("seed: ", seed, "temperature location: ", temperature_location)
+        # next, we need to find the temperature location
+        temperature_location = maps["light-to-temperature"].find_destination(light_location)
+        #print("seed: ", seed, "temperature location: ", temperature_location)
 
-    # next, we need to find the humidity location
-    humidity_location = maps["temperature-to-humidity"].find_destination(temperature_location)
-    #print("seed: ", seed, "humidity location: ", humidity_location)
+        # next, we need to find the humidity location
+        humidity_location = maps["temperature-to-humidity"].find_destination(temperature_location)
+        #print("seed: ", seed, "humidity location: ", humidity_location)
 
-    # finally, we need to find the location
-    location = maps["humidity-to-location"].find_destination(humidity_location)
-    #print("seed: ", seed, "location: ", location)
+        # finally, we need to find the location
+        location = maps["humidity-to-location"].find_destination(humidity_location)
+        #print("seed: ", seed, "location: ", location)
 
-    # print the location
-    #print("seed: ", seed, "location: ", location)
+        # check for lowest location seed
+        if location < lowest_location:
+            lowest_location = location
+            lowest_seed = seed
 
-    # check for lowest location seed
-    if location < lowest_location:
-        lowest_location = location
-        lowest_seed = seed
-
+    
 # close the file
 f.close()
 
