494102f1bba3285dea68d00402e3da0085c5077f
wallarug <wallarug@coliemore.com.au>
2023-12-09 21:33:20 +1100

    Day 5 Part 1 complete.  Format was: dest, src, range.

--- 5/activity1.py
+++ 5/activity1.py
@@ -4,7 +4,7 @@
 # Higher than: 
 # Less than: 
 # wrong: 
-# correct: 
+# correct: 251346198
 
 from sys import exit
 DEBUG = False
@@ -14,7 +14,7 @@ import os
 script_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_path)
 
-f = open("test.txt", "r")
+f = open("input.txt", "r")
 
 class SpecialMap:
     def __init__(self, desc):
@@ -40,7 +40,8 @@ class SpecialMap:
             # TODO: check inclusive/exclusive
             if (search_value <= start_s + r) and (search_value >= start_s):
                 # the value is in this range, return mapped value
-                return start_d + (search_value - start_s) 
+                offset = start_d - start_s
+                return search_value + offset 
         
         # the value does not confirm to a map, so just return the value (1:1 mapping)
         return search_value
@@ -79,14 +80,12 @@ for line in f.readlines():
                 seeds.remove(x)
 
         seeds = [int(x) for x in seeds]
-        print("seeds: ", seeds)
         continue
 
     # if it is not a seed, it is a map
     if "map" in line:
         data = line.strip("\n").split(":")
         map_name = data[0].strip(" ").split(" ")[0]
-        print("Map Name: ", map_name)
         current_map = maps[map_name]
         continue
 
@@ -95,44 +94,44 @@ for line in f.readlines():
 
     # this is where we input the data...
     data = line.strip("\n").split(" ")
-    current_map.add_to_map(int(data[0]), int(data[1]), int(data[2]))
+    current_map.add_to_map(int(data[1]), int(data[0]), int(data[2]))
     continue
 
 # Phase 2 - actually working out the seed locations
-lowest_location = 999999
+lowest_location = 999999999999999999999999999999999999
 lowest_seed = 0
 
 for seed in seeds:
     # first, we need to find the soil location
     soil_location = maps["seed-to-soil"].find_destination(seed)
-    print("seed: ", seed, "soil location: ", soil_location)
+    #print("seed: ", seed, "soil location: ", soil_location)
 
     # next, we need to find the fertilizer location
     fertilizer_location = maps["soil-to-fertilizer"].find_destination(soil_location)
-    print("seed: ", seed, "fertilizer location: ", fertilizer_location)
+    #print("seed: ", seed, "fertilizer location: ", fertilizer_location)
 
     # next, we need to find the water location
     water_location = maps["fertilizer-to-water"].find_destination(fertilizer_location)
-    print("seed: ", seed, "water location: ", water_location)
+    #print("seed: ", seed, "water location: ", water_location)
 
     # next, we need to find the light location
     light_location = maps["water-to-light"].find_destination(water_location)
-    print("seed: ", seed, "light location: ", light_location)
+    #print("seed: ", seed, "light location: ", light_location)
 
     # next, we need to find the temperature location
     temperature_location = maps["light-to-temperature"].find_destination(light_location)
-    print("seed: ", seed, "temperature location: ", temperature_location)
+    #print("seed: ", seed, "temperature location: ", temperature_location)
 
     # next, we need to find the humidity location
     humidity_location = maps["temperature-to-humidity"].find_destination(temperature_location)
-    print("seed: ", seed, "humidity location: ", humidity_location)
+    #print("seed: ", seed, "humidity location: ", humidity_location)
 
     # finally, we need to find the location
     location = maps["humidity-to-location"].find_destination(humidity_location)
-    print("seed: ", seed, "location: ", location)
+    #print("seed: ", seed, "location: ", location)
 
     # print the location
-    print("seed: ", seed, "location: ", location)
+    #print("seed: ", seed, "location: ", location)
 
     # check for lowest location seed
     if location < lowest_location:
