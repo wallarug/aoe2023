a2be88b8dd31eb0beb34e81d23885ddb0749ba76
wallarug <wallarug@coliemore.com.au>
2023-12-12 22:35:16 +1100

    template code for Day 9

---
+++ 9/activity1.py
@@ -0,0 +1,24 @@
+#!/bin/usr/python3
+# Day 9: Advent of Code
+
+# Higher than: 
+# Less than: 
+# wrong: 
+# correct: 
+
+from sys import exit
+DEBUG = False
+
+import os
+import math
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_path)
+
+answer = 0
+
+
+
+
+print("Answer: ", answer)
+exit()
---
+++ 9/activity2.py
@@ -0,0 +1,26 @@
+#!/bin/usr/python3
+# Day 9: Advent of Code (Part 2)
+
+# Higher than: 
+# Less than:  
+# wrong: 
+# correct: 
+
+from sys import exit
+DEBUG = False
+
+import os
+import math
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_path)
+
+answer = 0
+
+
+
+
+print("Answer: ", answer)
+exit()
+
+
