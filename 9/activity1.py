5f0cdf178b31ef5fa65939d56daf433d17873287
wallarug <wallarug@coliemore.com.au>
2023-12-12 23:27:48 +1100

    Day 9 Part 1 complete (1st attempt)

--- 9/activity1.py
+++ 9/activity1.py
@@ -14,11 +14,67 @@ import math
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_path)
+f = open("input.txt", "r")
+
+def differences(list):
+    diff = []
+    for i in range(0, len(list) - 1):
+        diff.append(list[i + 1] - list[i])
+    return diff
+
 
 answer = 0
+values = []
+temp_diffs = []
+temp_i = 0
+all_zero = False
+
+for line in f.readlines():
+    # parse line
+    line = line.strip().split(" ")
+    line = [int(i) for i in line]
+
+    # reset variables
+    temp_diffs = []
+    temp_i = 0
+    all_zero = False
+
+    # add line to temp_diffs
+    temp_diffs.append(line)    
+
+    # find differences
+    while(all_zero == False):
+        temp_diffs.append(differences(temp_diffs[temp_i]))
+        temp_i += 1
+
+        #print("Temp diffs (uno): ", temp_diffs)
+
+        for i in range(0, len(temp_diffs[temp_i])):
+            if temp_diffs[temp_i][i] == 0:
+                all_zero = True
+            else:
+                all_zero = False
+                break
+
+    #print("Differences: ", temp_diffs)
+    
+    # calculate next value
+    prev = 0
+    for i in range(len(temp_diffs)-1, 0, -1):
+        left = temp_diffs[i-1][-1]
+        right = left + prev
+        prev = right
+        #print("Left: ", left, "Right: ", right, "Prev: ", prev)
+
+    #print("Next value: ", prev)
+    values.append(prev)
 
 
+# sum all values
+for i in range(0, len(values)):
+    answer += values[i]
 
+f.close()
 
 print("Answer: ", answer)
 exit()
