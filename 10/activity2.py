b0db25c72c2ad42dbdfdc1e3debfd0738472dcf0
wallarug <wallarug@coliemore.com.au>
2023-12-12 23:48:35 +1100

    Day 10 templates.

---
+++ 10/activity2.py
@@ -0,0 +1,25 @@
+#!/bin/usr/python3
+# Day 10: Advent of Code (Part 2)
+
+# Higher than: 
+# Less than:  
+# wrong: 
+# correct: 
+
+from sys import exit
+DEBUG = False
+
+import os
+import math
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_path)
+f = open("input.txt", "r")
+
+answer = 0
+
+
+f.close()
+
+print("Answer: ", answer)
+exit()
